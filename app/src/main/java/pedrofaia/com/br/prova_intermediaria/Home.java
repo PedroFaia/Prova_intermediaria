6bcfa38777f78a98239089efeeaa9f999346d1ee
arqdsis <arqdsis@mocli10231.animaedu.intranet>
2017-10-26 20:45:21 -0200

    Initial commit

---
+++ app/src/main/java/pedrofaia/com/br/prova_intermediaria/Home.java
@@ -0,0 +1,13 @@
+package pedrofaia.com.br.prova_intermediaria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
